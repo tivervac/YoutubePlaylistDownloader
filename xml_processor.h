e0b593830c6581312c97158b0b8157ee5f172062
Titouan Vervack <tvervack@hotmail.com>
2013-02-17 22:18:07 +0100

    Deleted deprecated files

--- xml_processor.h
+++
@@ -1,9 +0,0 @@
-/********************************
-* Contains all methods to manipulate
-********************************/
-#ifndef XML_PROCESSOR_H
-#define XML_PROCESSOR_H
-#include "xml_buffer.h"
-
-
-#endif
